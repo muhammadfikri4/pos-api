724ce6e5ed79aae45234df66ec9da79fe8eadd2b
muhammadfikri4 <unicornhorse1@gmail.com>
2024-06-09 22:42:43 +0700

    fix: ignore dist

--- dist/app/authentication/authRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authController_1 = require("./authController");
-const route = (0, express_1.Router)();
-route.post("/register", authController_1.registerController);
-route.get("/login", authController_1.loginController);
-exports.default = route;
--- dist/utils/Regex.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REGEX = void 0;
-exports.REGEX = {
-    email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-    number: /^[0-9]+$/,
-};
