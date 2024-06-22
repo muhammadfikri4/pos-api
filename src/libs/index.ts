21a7ee132e7ca489caa018aefeb1ea06d1d8ccb9
Muhammad Fikri <unicornhorse1@gmail.com>
2024-06-22 19:07:37 +0700

    merging

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -6,9 +6,6 @@ export const ENV = {
     DB_URL: process.env.DB_URL,
     PORT: process.env.PORT,
     JWT_SECRET: process.env.JWT_SECRET,
-<<<<<<< HEAD
-
-=======
     DB_NAME: process.env.DB_NAME,
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD,
@@ -20,5 +17,4 @@ export const ENV = {
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME
->>>>>>> 8b33369cb626cd470f5e8319bb10a1fd89cb0bc2
 }
