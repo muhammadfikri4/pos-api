f05555b6717d06f8492a8e8f9d1a13071c9d6f39
Muhammad Fikri <unicornhorse1@gmail.com>
2024-06-22 13:40:34 +0700

    testing commit

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -6,10 +6,5 @@ export const ENV = {
     DB_URL: process.env.DB_URL,
     PORT: process.env.PORT,
     JWT_SECRET: process.env.JWT_SECRET,
-    DB_NAME: process.env.DB_NAME,
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    CLOUD_NAME: process.env.CLOUD_NAME,
-    API_KEY: process.env.API_KEY,
-    API_SECRET: process.env.API_SECRET
+
 }
