271f2fc5eadb22f76ffe2e20351115d722911c90
muhammadfikri4 <unicornhorse1@gmail.com>
2024-06-27 20:09:18 +0700

    remove cors

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv'
 import express from 'express'
 // import { dbconect } from './config'
 import bodyParser from 'body-parser'
-import cors from 'cors'
 import { ENV } from './libs'
 import routes from './routes'
 
@@ -11,7 +10,7 @@ const app = express()
 const port = ENV.PORT || 5000
 dotenv.config();
 // dbconect()
-app.use(cors())
+// app.use(cors())
 app.use(cookieParser())
 
 app.use(express.json());
