8b2b213e142ad1df101206eede67db07aa56af52
muhammadfikri4 <unicornhorse1@gmail.com>
2024-06-27 11:07:25 +0700

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import dotenv from 'dotenv'
 import express from 'express'
 // import { dbconect } from './config'
 import bodyParser from 'body-parser'
-import cors from 'cors'
-import { type Request } from 'express'
 import path from 'path'
 import { ENV } from './libs'
 import routes from './routes'
@@ -14,13 +12,14 @@ const port = ENV.PORT || 5000
 dotenv.config();
 // dbconect()
 app.use(cookieParser())
-app.use(cors({
-    origin: "https://pos-web-app-mu.vercel.app",
-    credentials: true,
-}))
-app.use((req: Request) => {
-    req.headers['access-control-allow-origin'] = "https://pos-web-app-mu.vercel.app"
-})
+// app.use(cors({
+//     origin: "https://pos-web-app-mu.vercel.app",
+//     credentials: true,
+// }))
+// app.use((req: Request) => {
+//     req.headers['access-control-allow-origin'] = "https://pos-web-app-mu.vercel.app"
+//     req.
+// })
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
