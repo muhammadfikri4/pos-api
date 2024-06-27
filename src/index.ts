55f30fe00c0a93dd6a79892f36c240af98d60e5d
muhammadfikri4 <unicornhorse1@gmail.com>
2024-06-27 13:52:55 +0700

    create middleware cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
-import express from 'express'
+import express, { type NextFunction, type Request, type Response } from 'express'
 // import { dbconect } from './config'
 import bodyParser from 'body-parser'
 import cors from 'cors'
@@ -19,7 +19,17 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header('Access-Control-Allow-Origin', 'https://pos-web-app-mu.vercel.app');
+    res.header('Access-Control-Allow-Methods', "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+    res.header('Access-Control-Allow-Headers', "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    };
+    next();
+});
 // app.use("/images", express.static(path.join(__dirname, "../src/images")));
 app.use(routes)
 
