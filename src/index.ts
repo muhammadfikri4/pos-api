0f6e00cf795574c0b897b2c89c13234050b89cb5
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-06-30 00:31:50 +0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ app.use(cookieParser())
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/api" , routes)
+app.use(routes)
 
 app.listen(port, () => {
     console.log(`Run at port ${port}🚀`)
