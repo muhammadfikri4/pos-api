ccc9a6c30d5a7d6961deadb0f4993357d2a3bd46
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-06-29 14:04:22 +0700

    fix: transaction

--- src/app/transaction/transactionValidate.ts
+++ src/app/transaction/transactionValidate.ts
@@ -1,73 +1,138 @@
-import { MESSAGE_CODE } from "../../utils/ErrorCode"
-import { AppError } from "../../utils/HttpError"
-import { MESSAGES } from "../../utils/Messages"
-import { getProductById } from "../product/productRepo"
-import { TransactionBodyDTO, TransactionDetailDTO } from "./transactionDTO"
-import { getTransactionById, getTransactionDetailById } from "./transactionRepo"
+import { getProductByIdService } from "app/product/productService";
+import { MESSAGE_CODE } from "../../utils/ErrorCode";
+import { AppError } from "../../utils/HttpError";
+import { MESSAGES } from "../../utils/Messages";
+import { getProductById } from "../product/productRepo";
+import { TransactionBodyDTO, TransactionDetailDTO } from "./transactionDTO";
+import {
+  getTransactionById,
+  getTransactionDetailById,
+} from "./transactionRepo";
 // import { getProductById, getProductByName } from "./transactionRepo"
 
-export const createTransactionValidate = async ({ email, name, paymentMethod, details }: TransactionBodyDTO) => {
-    if (!email) {
-        return AppError(MESSAGES.ERROR.REQUIRED.EMAIL, 400, MESSAGE_CODE.BAD_REQUEST)
-    }
-    if (!name) {
-        return AppError(MESSAGES.ERROR.REQUIRED.NAME, 400, MESSAGE_CODE.BAD_REQUEST)
-    }
-    if (!paymentMethod) {
-        return AppError(MESSAGES.ERROR.REQUIRED.NAME, 400, MESSAGE_CODE.BAD_REQUEST)
-    }
+export const createTransactionValidate = async ({
+  email,
+  name,
+  paymentMethod,
+  details,
+}: TransactionBodyDTO) => {
+  if (!email) {
+    return AppError(
+      MESSAGES.ERROR.REQUIRED.EMAIL,
+      400,
+      MESSAGE_CODE.BAD_REQUEST
+    );
+  }
+  if (!name) {
+    return AppError(
+      MESSAGES.ERROR.REQUIRED.NAME,
+      400,
+      MESSAGE_CODE.BAD_REQUEST
+    );
+  }
+  if (!paymentMethod) {
+    return AppError(
+      MESSAGES.ERROR.REQUIRED.NAME,
+      400,
+      MESSAGE_CODE.BAD_REQUEST
+    );
+  }
 
-    if (details?.length as number < 1) {
-        return AppError(MESSAGES.ERROR.INVALID.PRODUCT_ITEM, 400, MESSAGE_CODE.BAD_REQUEST)
-    }
+  if ((details?.length as number) < 1) {
+    return AppError(
+      MESSAGES.ERROR.INVALID.PRODUCT_ITEM,
+      400,
+      MESSAGE_CODE.BAD_REQUEST
+    );
+  }
 
-}
+  const productDetails = await Promise.all(
+    (details as TransactionDetailDTO[]).map((item) =>
+      getProductById(item.productId)
+    )
+  );
+  const hasInvalidProduct = productDetails.some((product) => product === null);
 
-export const createTransactionDetailValidate = async (details: TransactionDetailDTO[]) => {
+  if (hasInvalidProduct) {
+    return AppError(
+      MESSAGES.ERROR.INVALID.PRODUCT_ITEM,
+      400,
+      MESSAGE_CODE.BAD_REQUEST
+    );
+  }
 
-    const promises = details.map(async (item) => {
-        const getProduct = await getProductById(item?.productId);
-        return (item?.quantity as number) > (getProduct?.stock as number);
-    });
-    const result = await Promise.all(promises)
-    const findMinus = result.find((item) => item === true)
-    if (findMinus) {
+  // if (findName) {
+  //     // unlinkSync(`./src/images/products/${image}`)
+  //     return AppError(MESSAGES.ERROR.ALREADY.PRODUCT, 400, MESSAGE_CODE.BAD_REQUEST)
+  // }
+};
 
-        return AppError(MESSAGES.ERROR.INVALID.TRANSACTION_ORDER, 400, MESSAGE_CODE.BAD_REQUEST);
-    }
-
-}
+export const createTransactionDetailValidate = async (
+  details: TransactionDetailDTO[]
+) => {
+  const promises = details.map(async (item) => {
+    const getProduct = await getProductById(item?.productId);
+    return (item?.quantity as number) > (getProduct?.stock as number);
+  });
+  const result = await Promise.all(promises);
+  const findMinus = result.find((item) => item === true);
+  if (findMinus) {
+    return AppError(
+      MESSAGES.ERROR.INVALID.TRANSACTION_ORDER,
+      400,
+      MESSAGE_CODE.BAD_REQUEST
+    );
+  }
+};
 
 export const updateStatusToPaidTransactionValidate = async (id: string) => {
-    const findTransaction = await getTransactionById(id as string)
-    if (!findTransaction) {
-        return AppError(MESSAGES.ERROR.NOT_FOUND.TRANSACTION, 404, MESSAGE_CODE.NOT_FOUND)
-    }
-    const promises = findTransaction.transactionDetails.map(async (item) => {
-        const getById = await getTransactionDetailById(item.id)
-        const getProduct = await getProductById(getById?.productId)
+  const findTransaction = await getTransactionById(id as string);
+  if (!findTransaction) {
+    return AppError(
+      MESSAGES.ERROR.NOT_FOUND.TRANSACTION,
+      404,
+      MESSAGE_CODE.NOT_FOUND
+    );
+  }
+  const promises = findTransaction.transactionDetails.map(async (item) => {
+    const getById = await getTransactionDetailById(item.id);
+    const getProduct = await getProductById(getById?.productId);
 
-        return getProduct?.stock as number < (getById?.quantity as number)
-    })
-    const result = await Promise.all(promises)
-    const findMinus = result.find((item) => item === true)
-    if (findMinus) {
+    return (getProduct?.stock as number) < (getById?.quantity as number);
+  });
+  const result = await Promise.all(promises);
+  const findMinus = result.find((item) => item === true);
+  if (findMinus) {
+    return AppError(
+      MESSAGES.ERROR.INVALID.TRANSACTION_ORDER,
+      400,
+      MESSAGE_CODE.BAD_REQUEST
+    );
+  }
+};
 
-        return AppError(MESSAGES.ERROR.INVALID.TRANSACTION_ORDER, 400, MESSAGE_CODE.BAD_REQUEST)
-    }
-}
+export const updatePaymentTransactionValidate = async (
+  id: string,
+  totalPaid: number
+) => {
+  const findTransaction = await getTransactionById(id as string);
+  if (!findTransaction) {
+    return AppError(
+      MESSAGES.ERROR.NOT_FOUND.TRANSACTION,
+      404,
+      MESSAGE_CODE.NOT_FOUND
+    );
+  }
 
-export const updatePaymentTransactionValidate = async (id: string, totalPaid: number) => {
-    const findTransaction = await getTransactionById(id as string)
-    if (!findTransaction) {
-        return AppError(MESSAGES.ERROR.NOT_FOUND.TRANSACTION, 404, MESSAGE_CODE.NOT_FOUND)
-    }
-
-    const totalAmount = findTransaction?.totalAmount as number;
-    if (totalPaid < totalAmount) {
-        return AppError(MESSAGES.ERROR.PAYMENT.PAYMENT_MONEY, 400, MESSAGE_CODE.BAD_REQUEST)
-    }
-}
+  const totalAmount = findTransaction?.totalAmount as number;
+  if (totalPaid < totalAmount) {
+    return AppError(
+      MESSAGES.ERROR.PAYMENT.PAYMENT_MONEY,
+      400,
+      MESSAGE_CODE.BAD_REQUEST
+    );
+  }
+};
 
 // export const updateProductValidate = async ({ name, id, image }: ProductBodyDTO, size: number) => {
 //     const findUnique = await getProductById(id)
